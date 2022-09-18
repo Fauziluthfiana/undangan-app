3a759b975d98ef63a2a0064e6ca3da387c69b4af
Faujiluthfiana <faujiluthfiana@gmail.com>
2022-09-18 18:37:58 +0700

    Updates

---
+++ precache-manifest.7d4c956cd5d7a5bc43fa39f3876d6019.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "329f6fc0810875678b1a",
+    "url": "/undangan-app/static/css/main.64fb15be.chunk.css"
+  },
+  {
+    "revision": "329f6fc0810875678b1a",
+    "url": "/undangan-app/static/js/main.329f6fc0.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/undangan-app/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "d2c5eb60f28de718f757",
+    "url": "/undangan-app/static/css/2.868a7f55.chunk.css"
+  },
+  {
+    "revision": "d2c5eb60f28de718f757",
+    "url": "/undangan-app/static/js/2.d2c5eb60.chunk.js"
+  },
+  {
+    "revision": "c2e1bd0cfb9bc8df3e06",
+    "url": "/undangan-app/static/js/runtime~main.c2e1bd0c.js"
+  },
+  {
+    "revision": "3ccbdc65094ac474667abb2733c998b9",
+    "url": "/undangan-app/static/media/nn.3ccbdc65.png"
+  },
+  {
+    "revision": "31b7099a432ef028fceebdf7a8248775",
+    "url": "/undangan-app/static/media/amplop2.31b7099a.png"
+  },
+  {
+    "revision": "43183d42dc26ec8ebbae0d80837696ce",
+    "url": "/undangan-app/static/media/kado1.43183d42.png"
+  },
+  {
+    "revision": "0b654e5aaaefaeb6198054270e531ce6",
+    "url": "/undangan-app/static/media/bca.0b654e5a.png"
+  },
+  {
+    "revision": "4df11d14a05120b48b64820a4992a8eb",
+    "url": "/undangan-app/static/media/masker.4df11d14.png"
+  },
+  {
+    "revision": "b7f40dc090c6840ce78ca4607dd3d49b",
+    "url": "/undangan-app/static/media/jarak.b7f40dc0.png"
+  },
+  {
+    "revision": "a9aac1e00e70ea064584f737a8576e8d",
+    "url": "/undangan-app/static/media/vaksin.a9aac1e0.png"
+  },
+  {
+    "revision": "b2be7d5da79191c5ff6fb882d6c1fbab",
+    "url": "/undangan-app/static/media/you're the reason.b2be7d5d.mp3"
+  },
+  {
+    "revision": "d4431dc14928c71f68fe8f95696f32a1",
+    "url": "/undangan-app/static/media/Quintessential-Regular.d4431dc1.ttf"
+  },
+  {
+    "revision": "ca8b43c8d5c8ce977d715df0e3349512",
+    "url": "/undangan-app/static/media/Charm-Regular.ca8b43c8.ttf"
+  },
+  {
+    "revision": "e699322e5bff6e59675d5940e7f8633f",
+    "url": "/undangan-app/static/media/undangan.e699322e.png"
+  },
+  {
+    "revision": "f3995bdbb8f63e6733fc87d0a2bc3e52",
+    "url": "/undangan-app/static/media/border.f3995bdb.png"
+  },
+  {
+    "revision": "ab643f4a047d9c4021ceac727516b95b",
+    "url": "/undangan-app/static/media/sampul2.ab643f4a.png"
+  },
+  {
+    "revision": "33b677a427ac8cbdad3f6dc074e157af",
+    "url": "/undangan-app/static/media/pasangan1.33b677a4.jpeg"
+  },
+  {
+    "revision": "71ffdf39fa074f115c1ceb993183b256",
+    "url": "/undangan-app/static/media/nav4.71ffdf39.png"
+  },
+  {
+    "revision": "c3b7adceb8e4b6c82315fff527158bb6",
+    "url": "/undangan-app/static/media/penutup.c3b7adce.png"
+  },
+  {
+    "revision": "addcba71f6b19a2e9393774a35093cb4",
+    "url": "/undangan-app/static/media/akad1.addcba71.png"
+  },
+  {
+    "revision": "381ae3a018302c7ee56e7f7c2d7a8632",
+    "url": "/undangan-app/index.html"
+  }
+];
